6cce8fe8c25e8af52fb7fdcc993aaeaaf4afff38
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-23 01:17:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ class Bird {
         if (this.sex=="male") {
         return `It's me! ${this.name}, the parrot!`; }
         else {
-        `${this.name} says squawk!`; }
+        return `${this.name} says squawk!`; }
    }
 }
